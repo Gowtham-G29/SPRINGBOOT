e243d8a22ead029f2f8e5a0590ee4f38b2281547
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-05-14 00:09:40 +0530

    Getting started with SPRING DATA JPA and Hibernate.

--- src/main/java/com/g_29/FirstApp/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/g_29/FirstApp/CourseJdbcCommandLineRunner.java
@@ -1,7 +1,7 @@
 package com.g_29.FirstApp;
 
 import com.g_29.FirstApp.model.Course;
-import com.g_29.FirstApp.repository.CourseJpaRepository;
+import com.g_29.FirstApp.repository.CourseSpringDataJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,19 +12,26 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepo repo;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJPARepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1,"learn AWS ","g_29"));
-        repository.insert(new Course(2,"learn Azure ","g_29"));
-        repository.insert(new Course(3,"learn Gcp ","g_29"));
+        repository.save(new Course(1,"learn AWS ","g_29"));
+        repository.save(new Course(2,"learn Azure ","g_29"));
+        repository.save(new Course(3,"learn Gcp ","g_29"));
 
-        repository.findById(1);
+        repository.findById(1L);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+        System.out.println(repository.findAll());
+
+        System.out.println(repository.findByAuthor("g_29"));
+        System.out.println(repository.findByName("learn AWS "));
 
     }
 }
